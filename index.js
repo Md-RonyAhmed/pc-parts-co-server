72e398a2cbe01b71fba7a8b5a58053be3c68b26f
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-25 08:38:44 +0600

    verify jwt

--- index.js
+++ index.js
@@ -16,6 +16,22 @@ app.options("*", cors(corsConfig));
 
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "UnAuthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 // DB connection
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.ntftt.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -41,7 +57,7 @@ const client = new MongoClient(uri, {
       });
     });
 
-    app.get("/blogs", async (req, res) => {
+    app.get("/blogs",verifyJWT, async (req, res) => {
       const query = {};
       const blogs = await blogsCollection.find(query).toArray();
       if (!blogs?.length) {
