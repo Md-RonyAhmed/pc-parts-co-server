37233471842136f88fef2372372b74c6821ec719
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-25 07:06:16 +0600

    DB connected

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -15,8 +16,19 @@ app.options("*", cors(corsConfig));
 
 app.use(express.json());
 
+// DB connection
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.ntftt.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 (async () => {
-  
+  await client.connect();
+  const productCollection = client.db("pc-partsDb").collection("pc-parts");
+  console.log('DB connected');
 })().catch(console.dir);
 
 app.get('/', (req, res) => {
