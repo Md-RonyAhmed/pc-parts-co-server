255a52bf2034e3b9f97ab1e9f899b0453db8a62b
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-27 02:20:21 +0600

    get user update

--- index.js
+++ index.js
@@ -86,6 +86,23 @@ const client = new MongoClient(uri, {
       res.send(users);
     });
 
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "6h" }
+      );
+      res.send({ result, token });
+    });
+
 
   } catch (error) {
     console.log(error);
