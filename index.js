47b3873198540f0b1e55c99a02a77411cede9149
Md. Rony <rony15-10527@diu.edu.bd>
2022-05-28 20:13:57 +0600

    make admin

--- index.js
+++ index.js
@@ -90,6 +90,13 @@ const client = new MongoClient(uri, {
       res.send(users);
     });
 
+     app.get("/admin/:email", async (req, res) => {
+       const email = req.params.email;
+       const user = await userCollection.findOne({ email: email });
+       const isAdmin = user.role === "admin";
+       res.send({ admin: isAdmin });
+     });
+
      app.put("/user/admin/:email", verifyJWT, async (req, res) => {
        const email = req.params.email;
        const filter = { email: email };
